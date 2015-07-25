af467381ca44028e85990858b94f8d985164b4f7
Daniel Lovasko <daniel.lovasko@gmail.com>
2015-07-25 15:56:02 +0200

    whitespace alignment

--- src/m_trie.h
+++ src/m_trie.h
@@ -19,23 +19,28 @@ struct m_trie
 	uint8_t children_count;
 };
 
-#define M_TRIE_OK 0
-#define M_TRIE_E_NULL 1
-#define M_TRIE_E_NOT_FOUND 2
-#define M_TRIE_E_KEY_INVALID 3
-#define M_TRIE_E_EXISTS 4
-#define M_TRIE_E_COPY_INVALID 5
+#define M_TRIE_OK                  0
+#define M_TRIE_E_NULL              1
+#define M_TRIE_E_NOT_FOUND         2
+#define M_TRIE_E_KEY_INVALID       3
+#define M_TRIE_E_EXISTS            4
+#define M_TRIE_E_COPY_INVALID      5
 #define M_TRIE_E_OVERWRITE_INVALID 6
-#define M_TRIE_E_MAX 6
+#define M_TRIE_E_MAX               6
 
-#define M_TRIE_OVERWRITE_ALLOW 0
+#define M_TRIE_OVERWRITE_ALLOW   0
 #define M_TRIE_OVERWRITE_PREVENT 1
 
-#define M_TRIE_COPY_DEEP 0
+#define M_TRIE_COPY_DEEP    0
 #define M_TRIE_COPY_SHALLOW 1
 
 int m_trie_init(struct m_trie* trie, int16_t (*hash)(char));
-int m_trie_set(struct m_trie* trie, char* key, uint8_t copy, uint8_t overwrite, void* data, size_t size);
+int m_trie_set(struct m_trie* trie,
+               char* key,
+               uint8_t copy,
+               uint8_t overwrite,
+               void* data,
+               size_t size);
 int m_trie_get(struct m_trie* trie, char* key, void** data);
 int m_trie_keys(struct m_trie* trie, struct m_list** out_keys);
 int m_trie_values(struct m_trie* trie, struct m_list** out_values);

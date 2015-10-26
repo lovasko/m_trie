73473317661557224df6b430199e2db54cc10206
Daniel Lovasko <daniel.lovasko@gmail.com>
2015-10-26 23:27:23 +0100

    zero is valid hash function result

--- src/init.c
+++ src/init.c
@@ -14,7 +14,7 @@ m_trie_init(struct m_trie* trie, int16_t (*hash)(char))
 	trie->hash = hash;
 	trie->children_count = 0;
 	for (i = CHAR_MIN; i < CHAR_MAX; i++)
-		if (hash(i) > 0)
+		if (hash(i) >= 0)
 			trie->children_count++;
 
 	node_init((struct __m_node**)&trie->root, trie->children_count);

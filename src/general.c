465a8fd0bfc98b81c6c16c5ff944edfd99ca6d8e
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-07-31 15:45:13 +0100

    added docs and removed error string function

--- src/general.c
+++ src/general.c
@@ -3,26 +3,58 @@
 #include "m_trie.h"
 #include "node.h"
 
+/**
+  * Initialise the trie.
+  *
+  * @param[in] trie trie
+  * @param[in] hash hash function
+  * @param[in] owrt overwrite mode
+  *
+  * @return status code
+  * @retval M_TRIE_E_NULL tr and/or hash is NULL
+  * @retval M_TRIE_OK     success
+**/
 int
-m_trie_init(m_trie* trie, int16_t (*hash)(char), uint8_t aux_store)
+m_trie_init(m_trie* trie, int16_t (*hash)(char), uint8_t owrt)
 {
 	char i;
 
 	if (trie == NULL || hash == NULL)
 		return M_TRIE_E_NULL;
 
-	trie->aux_store = aux_store;
+	if (owrt != M_TRIE_OVERWRITE_ALLOW
+	 && owrt != M_TRIE_OVERWRITE_PREVENT)
+	  return M_TRIE_E_UNKNOWN;
 
+	trie->owrt = owrt;
 	trie->hash = hash;
-	trie->children_count = 0;
+	trie->ccnt = 0;
 	for (i = CHAR_MIN; i < CHAR_MAX; i++)
 		if (hash(i) >= 0)
-			trie->children_count++;
+			trie->ccnt++;
 
-	node_init((struct __m_node**)&trie->root);
+	node_init((struct _node**)&trie->root);
 
-	m_list_init(&trie->keys);
-	m_list_init(&trie->values);
+	return M_TRIE_OK;
+}
+
+/**
+  * Free all memory resources held by the trie.
+  *
+  * @param[in] trie trie
+  * 
+  * @return status code
+  * @retval M_TRIE_E_NULL tr is NULL
+  * @retval M_TRIE_OK     success
+**/
+int
+m_trie_free(m_trie* trie)
+{
+	if (trie == NULL)
+		return M_TRIE_E_NULL;
+
+	m_trie_remove_all(trie);
+	node_free(trie->root);
 
 	return M_TRIE_OK;
 }

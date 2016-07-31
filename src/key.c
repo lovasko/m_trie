4795b8e0bb21f86b937fbb7f38efe83eb33ea9c3
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-07-31 15:28:05 +0100

    docs for keys; uint32_t -> size_t

--- src/key.c
+++ src/key.c
@@ -1,14 +1,26 @@
 #include "key.h"
 
+/**
+  * Check whether a key is valid.
+  * NOTE: this function is for internal use only.
+  *
+  * @param trie trie
+  * @param key  key
+  * @param len  key length
+  *
+  * @return key validity
+  * @retval 0 key is invalid
+  * @retval 1 key is valid
+**/
 int
-key_valid(m_trie* trie, char* key, uint32_t key_length)
+key_valid(m_trie* trie, char* key, size_t len)
 {
-	uint32_t i;
+	size_t i;
 
-	for (i = 0; i < key_length; i++)
+	for (i = 0; i < len; i++)
 		if (trie->hash(key[i]) < 0)
 			return 0;
-			
+
 	return 1;
 }
 
--- src/key.h
+++ src/key.h
@@ -1,11 +1,9 @@
 #ifndef M_TRIE_KEY_H
 #define M_TRIE_KEY_H
 
-#include <stdint.h>
-
 #include "m_trie.h"
 
-int key_valid(m_trie* trie, char* key, uint32_t key_length);
+int key_valid(m_trie* trie, char* key, size_t len);
 
 #endif
 

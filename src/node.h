6edafee7de035d2814bdc5dc44ce728354c12352
Daniel Lovasko <lovasko@freebsd.org>
2016-08-05 14:05:26 +0100

    documentation

--- src/node.h
+++ src/node.h
@@ -6,11 +6,12 @@
 #define M_TRIE_NODE_TYPE_DATA 0
 #define M_TRIE_NODE_TYPE_REGULAR 1
 
+/** Trie internal node. */
 struct _node {
-  void* data;
-  struct _node** chld;
-  char key;
-  uint8_t type;
+  void* data;          /**< Pointer to stored value. */
+  struct _node** chld; /**< Children nodes. */
+  char key;            /**< Node key. */
+  uint8_t type;        /**< DATA or REGULAR. */
   char pad[sizeof(void*) - 2];
 };
 

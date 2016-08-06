60b9ba8f2df974ce430c5e8e72a4ea77e3f19ba6
Daniel Lovasko <lovasko@freebsd.org>
2016-08-06 18:13:02 +0100

    changed tabs to spaces

--- src/general.c
+++ src/general.c
@@ -18,26 +18,27 @@
 int
 m_trie_init(m_trie* trie, int16_t (*hash)(char), uint8_t owrt)
 {
-	char i;
+  char i;
 
-	if (trie == NULL || hash == NULL)
-		return M_TRIE_E_NULL;
+  if (trie == NULL || hash == NULL)
+    return M_TRIE_E_NULL;
 
-	if (owrt != M_TRIE_OVERWRITE_ALLOW
-	 && owrt != M_TRIE_OVERWRITE_PREVENT)
-		return M_TRIE_E_UNKNOWN;
+  if (owrt != M_TRIE_OVERWRITE_ALLOW
+   && owrt != M_TRIE_OVERWRITE_PREVENT)
+    return M_TRIE_E_UNKNOWN;
 
-	trie->owrt = owrt;
-	trie->hash = hash;
-	trie->maxl = 0;
-	trie->ccnt = 0;
-	for (i = CHAR_MIN; i < CHAR_MAX; i++)
-		if (hash(i) >= 0)
-			trie->ccnt++;
+  trie->owrt = owrt;
+  trie->hash = hash;
+  trie->ncnt = 0;
+  trie->ccnt = 0;
 
-	node_init(trie, (struct _node**)&trie->root);
+  for (i = CHAR_MIN; i < CHAR_MAX; i++)
+    if (hash(i) >= 0)
+      trie->ccnt++;
 
-	return M_TRIE_OK;
+  node_init(trie, (struct _node**)&trie->root);
+
+  return M_TRIE_OK;
 }
 
 /**
@@ -52,12 +53,12 @@ m_trie_init(m_trie* trie, int16_t (*hash)(char), uint8_t owrt)
 int
 m_trie_free(m_trie* trie)
 {
-	if (trie == NULL)
-		return M_TRIE_E_NULL;
+  if (trie == NULL)
+    return M_TRIE_E_NULL;
 
-	m_trie_remove_all(trie);
-	node_free(trie, (struct _node**)&trie->root);
+  m_trie_remove_all(trie);
+  node_free(trie, (struct _node**)&trie->root);
 
-	return M_TRIE_OK;
+  return M_TRIE_OK;
 }
 

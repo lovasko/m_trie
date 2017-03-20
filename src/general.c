0b2b076560a51aaf03ecfae7e682c38c0085d9f9
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-03-20 07:15:57 +0000

    added commentary; fixed missing content of the free function

--- src/general.c
+++ src/general.c
@@ -3,51 +3,49 @@
 #include "m_trie.h"
 #include "node.h"
 
-/**
-  * Initialise the trie.
+
+/** Initialise the trie.
   *
-  * @param[in] trie trie
-  * @param[in] hash hash function
-  * @param[in] owrt overwrite mode
+  * @param[in] trie  trie
+  * @param[in] hash  hash function
+  * @param[in] flags behaviour flags
   *
   * @return status code
-  * @retval M_TRIE_E_NULL    tr and/or hash is NULL
-  * @retval M_TRIE_E_UNKNOWN no such overwrite mode
-  * @retval M_TRIE_OK        success
+  * @retval M_TRIE_E_NULL trie and/or hash is NULL
+  * @retval M_TRIE_OK     success
 **/
 int
-m_trie_init(m_trie* trie, int16_t (*hash)(char), uint8_t owrt)
+m_trie_init(m_trie* trie, short (*hash)(char), int flags)
 {
   char i;
 
   if (trie == NULL || hash == NULL)
     return M_TRIE_E_NULL;
 
-  if (owrt != M_TRIE_OVERWRITE_ALLOW
-   && owrt != M_TRIE_OVERWRITE_PREVENT)
-    return M_TRIE_E_UNKNOWN;
+  trie->tr_flags = (uint8_t)flags;
+  trie->tr_hash = hash;
+  trie->tr_ncnt = 0;
 
-  trie->owrt = owrt;
-  trie->hash = hash;
-  trie->ncnt = 0;
-  trie->ccnt = 0;
-
+  /* Compute the children count, by counting the number of positive
+   * answers from the hash function. */
+  trie->tr_ccnt = 0;
   for (i = CHAR_MIN; i < CHAR_MAX; i++)
     if (hash(i) >= 0)
-      trie->ccnt++;
+      trie->tr_ccnt++;
 
-  node_init(trie, (struct _node**)&trie->root);
+  /* Initialise the root node. */
+  node_init(trie, (node**)&trie->tr_root);
 
   return M_TRIE_OK;
 }
 
-/**
-  * Free all memory resources held by the trie.
+/** Free all memory resources held by the trie. Subsequent initialisation
+  * and use of the trie is allowed.
   *
   * @param[in] trie trie
   *
   * @return status code
-  * @retval M_TRIE_E_NULL tr is NULL
+  * @retval M_TRIE_E_NULL trie is NULL
   * @retval M_TRIE_OK     success
 **/
 int
@@ -56,9 +54,9 @@ m_trie_free(m_trie* trie)
   if (trie == NULL)
     return M_TRIE_E_NULL;
 
-  m_trie_remove_all(trie);
-  node_free(trie, (struct _node**)&trie->root);
+  m_trie_remove_prefix(trie, NULL, 0);
+  m_trie_trim(trie);
+  node_free(trie, (node*)trie->tr_root);
 
   return M_TRIE_OK;
 }
-

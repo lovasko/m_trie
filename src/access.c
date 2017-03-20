a5c40f4d03316a597da600647d2598f74220af1a
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-03-20 07:07:47 +0000

    formatting changes; added comments

--- src/access.c
+++ src/access.c
@@ -2,93 +2,99 @@
 #include "common.h"
 #include "node.h"
 
-/**
-  * Search for a key in the trie.
-  *
-  * @param[in]  trie  trie
-  * @param[in]  key   key
-  * @param[in]  len   key length
-  * @param[out] val   data associated with they key
-  *
-  * @returns status code
-  * @retval M_TRIE_E_NULL      trie and/or key is NULL
-  * @retval M_TRIE_E_LENGTH    length is zero
-  * @retval M_TRIE_E_INVALID   key contains invalid characters
-  * @retval M_TRIE_E_NOT_FOUND key is not in the trie
-  * @retval M_TRIE_OK          success
+/** Search for a key in the trie.
+ *
+ * @param[in]  trie trie
+ * @param[in]  key  key
+ * @param[in]  len  key length
+ * @param[out] val  data associated with the key
+ *
+ * @return status code
+ * @retval M_TRIE_E_NULL      trie and/or key is NULL
+ * @retval M_TRIE_E_LENGTH    length is zero
+ * @retval M_TRIE_E_INVALID   key contains invalid characters
+ * @retval M_TRIE_E_NOT_FOUND key is not in the trie
+ * @retval M_TRIE_OK          success
 **/
 int
 m_trie_search(m_trie* trie, char* key, size_t len, void** val)
 {
-	struct _node* node;
-	int ret;
+  node* nd;
+  int ret;
 
-	ret = locate(trie, key, len, &node);
-	if (ret != M_TRIE_OK)
-		return ret;
+  /* Locate the inner node of the trie. */
+  ret = locate(trie, key, len, &nd);
+  if (ret != M_TRIE_OK)
+    return ret;
 
-	if (node->type != M_TRIE_NODE_TYPE_DATA)
-		return M_TRIE_E_NOT_FOUND;
+  /* Check whether the node contains data. */
+  if (nd->nd_type != NODE_DATA)
+    return M_TRIE_E_NOT_FOUND;
 
-	if (val != NULL)
-		*val = node->data;
+  /* Return the value based on the caller's interest. */
+  if (val != NULL)
+    *val = nd->nd_data;
 
-	return M_TRIE_OK;
+  return M_TRIE_OK;
 }
 
-/**
-  * Insert a value into the trie.
-  *
-  * @param[in] trie trie
-  * @param[in] key  key
-  * @param[in] len  key length
-  * @param[in] val  value
-  *
-  * @return status code
-  * @retval M_TRIE_E_NULL    trie and/or key is NULL
-  * @retval M_TRIE_E_LENGTH  key length is zero
-  * @retval M_TRIE_E_INVALID key contains an invalid character
-  * @retval M_TRIE_E_EXISTS  key exists (only when overwriting is not allowed)
-  * @retval M_TRIE_OK        success
+/** Insert a value into the trie.
+ *
+ * @param[in] trie trie
+ * @param[in] key  key
+ * @param[in] len  key length
+ * @param[in] val  data associated with the key
+ *
+ * @return status code
+ * @retval M_TRIE_E_NULL    trie and/or key is NULL
+ * @retval M_TRIE_E_LENGTH  key length is zero
+ * @retval M_TRIE_E_INVALID key contains an invalid character
+ * @retval M_TRIE_E_EXISTS  key exists (only when overwriting is not allowed)
+ * @retval M_TRIE_OK        success
 **/
 int
 m_trie_insert(m_trie* trie, char* key, size_t len, void* val)
 {
-	size_t i;
-	int16_t h;
-	int ret;
-	struct _node* node;
+  size_t i;
+  int ret;
+  short h;
+  node* nd;
 
-	ret = check(trie, key, len);
-	if (ret != M_TRIE_OK)
-		return ret;
+  /* Validate the key. */
+  ret = check(trie, key, len);
+  if (ret != M_TRIE_OK)
+    return ret;
 
-	node = trie->root;
+  nd = trie->tr_root;
 
-	for (i = 0; i < len; i++) {
-		h = trie->hash(key[i]);
+  /* Traverse the key. */
+  for (i = 0; i < len; i++) {
 
-		if (node->chld == NULL)
-			node_chld(trie, &node);
+    /* Remove the removal mark from the node. */
+    if (nd->nd_type == NODE_TO_FREE)
+      nd->nd_type = NODE_REGULAR;
 
-		if (node->chld[h] == NULL) {
-			node_init(trie, &node->chld[h]);
-			node->chld[h]->key = key[i];
-		}
+    /* Create new subtree. */
+    if (nd->nd_chld == NULL)
+      node_chld(trie, &nd);
 
-		node = node->chld[h];
-	}
+    /* Initialise the needed node. */
+    h = trie->tr_hash(key[i]);
+    if (nd->nd_chld[h] == NULL) {
+      node_init(trie, &nd->nd_chld[h]);
+      nd->nd_chld[h]->nd_key = key[i];
+    }
 
-	if (node->type == M_TRIE_NODE_TYPE_DATA
-	 && trie->owrt == M_TRIE_OVERWRITE_PREVENT)
-		return M_TRIE_E_EXISTS;
+    /* Advance to the selected/created node. */
+    nd = nd->nd_chld[h];
+  }
 
-	node->type = M_TRIE_NODE_TYPE_DATA;
-	node->data = val;
+  /* Raise an error if the value can not be overwritten. */
+  if (nd->nd_type == NODE_DATA && (trie->tr_flags & M_TRIE_OVERWRITE))
+    return M_TRIE_E_EXISTS;
 
-	if (trie->maxl < len)
-		trie->maxl = len;
+  nd->nd_type = NODE_DATA;
+  nd->nd_data = val;
 
-	return M_TRIE_OK;
+  return M_TRIE_OK;
 }
-

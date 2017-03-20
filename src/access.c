4eac4b9e21086eef88cb1894c3ed5925a8231b03
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-03-20 07:17:13 +0000

    added offset space to function comments

--- src/access.c
+++ src/access.c
@@ -4,18 +4,18 @@
 
 
 /** Search for a key in the trie.
- *
- * @param[in]  trie trie
- * @param[in]  key  key
- * @param[in]  len  key length
- * @param[out] val  data associated with the key
- *
- * @return status code
- * @retval M_TRIE_E_NULL      trie and/or key is NULL
- * @retval M_TRIE_E_LENGTH    length is zero
- * @retval M_TRIE_E_INVALID   key contains invalid characters
- * @retval M_TRIE_E_NOT_FOUND key is not in the trie
- * @retval M_TRIE_OK          success
+  *
+  * @param[in]  trie trie
+  * @param[in]  key  key
+  * @param[in]  len  key length
+  * @param[out] val  data associated with the key
+  *
+  * @return status code
+  * @retval M_TRIE_E_NULL      trie and/or key is NULL
+  * @retval M_TRIE_E_LENGTH    length is zero
+  * @retval M_TRIE_E_INVALID   key contains invalid characters
+  * @retval M_TRIE_E_NOT_FOUND key is not in the trie
+  * @retval M_TRIE_OK          success
 **/
 int
 m_trie_search(m_trie* trie, char* key, size_t len, void** val)
@@ -40,18 +40,18 @@ m_trie_search(m_trie* trie, char* key, size_t len, void** val)
 }
 
 /** Insert a value into the trie.
- *
- * @param[in] trie trie
- * @param[in] key  key
- * @param[in] len  key length
- * @param[in] val  data associated with the key
- *
- * @return status code
- * @retval M_TRIE_E_NULL    trie and/or key is NULL
- * @retval M_TRIE_E_LENGTH  key length is zero
- * @retval M_TRIE_E_INVALID key contains an invalid character
- * @retval M_TRIE_E_EXISTS  key exists (only when overwriting is not allowed)
- * @retval M_TRIE_OK        success
+  *
+  * @param[in] trie trie
+  * @param[in] key  key
+  * @param[in] len  key length
+  * @param[in] val  data associated with the key
+  *
+  * @return status code
+  * @retval M_TRIE_E_NULL    trie and/or key is NULL
+  * @retval M_TRIE_E_LENGTH  key length is zero
+  * @retval M_TRIE_E_INVALID key contains an invalid character
+  * @retval M_TRIE_E_EXISTS  key exists (only when overwriting is not allowed)
+  * @retval M_TRIE_OK        success
 **/
 int
 m_trie_insert(m_trie* trie, char* key, size_t len, void* val)

0024d94b4f4c20e5a7054922f1b0751dc109bf7b
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-03-20 07:02:04 +0000

    new struct member names; support for behaviour flags

--- src/m_trie.h
+++ src/m_trie.h
@@ -2,7 +2,7 @@
 #define M_TRIE_H
 
 #include <stdlib.h>
-#include <stdint.h>
+
 
 #define M_TRIE_OK          0
 #define M_TRIE_E_NULL      1
@@ -14,21 +14,20 @@
 #define M_TRIE_E_UNKNOWN   7
 #define M_TRIE_E_NO_VALUE  8
 
-#define M_TRIE_OVERWRITE_ALLOW   0
-#define M_TRIE_OVERWRITE_PREVENT 1
+#define M_TRIE_OVERWRITE 1
+#define M_TRIE_CLEANUP   2
 
 /** Main trie object. */
 typedef struct m_trie {
-  void* root;            /**< Root node.                  */
-  int16_t (*hash)(char); /**< Tree-level hash function.   */
-  size_t ncnt;           /**< Overall node count.         */
-  uint8_t ccnt;          /**< Each node's children count. */
-  uint8_t owrt;          /**< Overwrite policy.           */
-  char pad[sizeof(void*)-2];
+  void*   tr_root;        /**< Root tree node.                  */
+  short (*tr_hash)(char); /**< Tree-level hash function.        */
+  size_t  tr_ncnt;        /**< Overall node count.              */
+  int     tr_ccnt;        /**< Maximal children count per node. */
+  int     tr_flags;       /**< Behaviour flags.                 */
 } m_trie;
 
 /* general */
-int m_trie_init(m_trie* trie, int16_t (*hash)(char), uint8_t owrt);
+int m_trie_init(m_trie* trie, short (*hash)(char), int flags);
 int m_trie_free(m_trie* trie);
 
 /* access */
@@ -50,4 +49,3 @@ int16_t m_trie_hash_lower_alphabet(char key);
 int16_t m_trie_hash_upper_alphabet(char key);
 
 #endif
-

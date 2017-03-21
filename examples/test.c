806bbde6ec4777a0cf6392f2bddb4d26ec2dd929
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-03-21 07:53:12 +0000

    applied new API to the test program

--- examples/test.c
+++ examples/test.c
@@ -4,40 +4,45 @@
 #include <unistd.h>
 #include <m_trie.h>
 
+
 int
 main(int argc, char* argv[])
 {
   m_trie trie;
-  int option;
+  int opt;
 
-  m_trie_init(&trie, m_trie_hash_lower_alphabet, M_TRIE_OVERWRITE_ALLOW);
+  m_trie_init(&trie, m_trie_hash_lower_alphabet, M_TRIE_OVERWRITE);
 
-  while ((option = getopt(argc, argv, "ai:p:r:s:t")) != -1) {
-    switch(option) {
+  while ((opt = getopt(argc, argv, "ai:np:r:s:t")) != -1) {
+    switch(opt) {
       case 'a':
         m_trie_remove_prefix(&trie, NULL, 0);
         printf("Removing all\n");
-      break;
+        break;
 
       case 'i':
         m_trie_insert(&trie, optarg, strlen(optarg), NULL);
         printf ("Inserting \"%s\"\n", optarg);
-      break;
+        break;
+
+      case 'n':
+        printf("Node count: %zu\n", trie.tr_ncnt);
+        break;
 
       case 'p':
         m_trie_remove_prefix(&trie, optarg, strlen(optarg));
         printf("Removing prefix \"%s\"\n", optarg);
-      break;
+        break;
 
       case 'r':
         m_trie_remove(&trie, optarg, strlen(optarg));
         printf("Removing \"%s\"\n", optarg);
-      break;
+        break;
 
       case 't':
         m_trie_trim(&trie);
         printf("Trimming \n");
-      break;
+        break;
 
       case 's':
         printf("Searching for \"%s\": ", optarg);
@@ -45,16 +50,15 @@ main(int argc, char* argv[])
           printf("Found\n");
         else
           printf("Not found\n");
-      break;
+        break;
 
       default: 
         fprintf(stderr, "ERROR: invalid arguments\n");
-      return EXIT_FAILURE;
+        return EXIT_FAILURE;
     }
-
-    printf("Node count: %u\n", trie.ncnt);
   }
+
+  m_trie_free(&trie);
 
   return EXIT_SUCCESS;
 }
-

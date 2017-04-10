500704d61200f31ca951c603e7a28510d8c25667
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-04-10 05:52:25 +0100

    improved comments in main header

--- src/m_trie.h
+++ src/m_trie.h
@@ -5,6 +5,7 @@
 #include <stdint.h>
 
 
+/* Return codes. */
 #define M_TRIE_OK          0
 #define M_TRIE_E_NULL      1
 #define M_TRIE_E_NOT_FOUND 2
@@ -15,11 +16,12 @@
 #define M_TRIE_E_UNKNOWN   7
 #define M_TRIE_E_NO_VALUE  8
 
+/* Behaviour flags. */
 #define M_TRIE_OVERWRITE 1
 #define M_TRIE_CLEANUP   2
 #define M_TRIE_FREE      4
 
-/** Main trie object. */
+/** Trie. */
 typedef struct m_trie {
   void*     tr_root;           /**< Root tree node.                  */
   int16_t (*tr_hash)(uint8_t); /**< Tree-level hash function.        */
@@ -30,20 +32,20 @@ typedef struct m_trie {
   uint8_t   tr_pad[2];         /**< Structure padding.               */
 } m_trie;
 
-/* general */
+/* General. */
 int m_trie_init(m_trie* tr, int16_t (*hash)(uint8_t), uint8_t flags);
 int m_trie_free(m_trie* tr);
 
-/* access */
+/* Access. */
 int m_trie_search(m_trie* tr, uint8_t* key, uint32_t len, void** val);
 int m_trie_insert(m_trie* tr, uint8_t* key, uint32_t len, void* val);
 
-/* remove */
+/* Removal. */
 int m_trie_remove(m_trie* tr, uint8_t* key, uint32_t len, uint8_t pfix);
 int m_trie_remove_all(m_trie *tr);
 int m_trie_trim(m_trie* tr);
 
-/* hash */
+/* Hashing. */
 int16_t m_trie_hash_identity(uint8_t key);
 int16_t m_trie_hash_alphabet(uint8_t key);
 int16_t m_trie_hash_digits(uint8_t key);

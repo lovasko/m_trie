f72a1d052b4f0cc44dd8c17d6dd2b60188cb39da
Daniel Lovasko <daniel.lovasko@gmail.com>
2018-12-03 08:25:42 +0000

    legal headers in all source files

--- src/node.h
+++ src/node.h
@@ -1,3 +1,9 @@
+// Copyright (c) 2015-2018 Daniel Lovasko
+// All Rights Reserved
+//
+// Distributed under the terms of the 2-clause BSD License. The full
+// license is in the file LICENSE, distributed as part of this software.
+
 #ifndef M_TRIE_NODE_H
 #define M_TRIE_NODE_H
 
